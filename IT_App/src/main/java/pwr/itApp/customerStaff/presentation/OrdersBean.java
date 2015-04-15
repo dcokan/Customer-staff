8bd321938c7143afe6b816a18be400f9fd8be305
Grzegorz Drzewinski <grzegorz.drzewinski@gmail.com>
2015-04-16 00:47:12 +0200

    New Order functionality DRAFT

--- IT_App/src/main/java/pwr/itApp/customerStaff/presentation/OrdersBean.java
+++ IT_App/src/main/java/pwr/itApp/customerStaff/presentation/OrdersBean.java
@@ -3,6 +3,7 @@ package pwr.itApp.customerStaff.presentation;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 
 import pwr.itApp.customerStaff.presentation.components.ElementsList;
 import pwr.itApp.customerStaff.presentation.dto.OrderDTO;
+import pwr.itApp.customerStaff.presentation.dto.ResourceDTO;
 import pwr.itApp.customerStaff.webapp.login.Actor;
 
 @ManagedBean(name="ordersBean")
@@ -19,6 +21,18 @@ public class OrdersBean implements ElementsList<OrderDTO> {
 
 	@Autowired
 	private Actor actor;
+
+    private List<OrderDTO> orderList;
+    private OrderDTO selectedOrder;
+    private OrderDTO newOrder;
+    private boolean newOrderAddMode;
+    private int activeTabIndex;
+
+    @PostConstruct
+    public void init() {
+        activeTabIndex = 0;
+        newOrder = new OrderDTO();
+    }
 	
     @Override
     public List<OrderDTO> getValueList() {
@@ -30,13 +44,49 @@ public class OrdersBean implements ElementsList<OrderDTO> {
 
 	@Override
 	public void onNewItemButton() {
-		throw new UnsupportedOperationException("New entry is not acceptable");
+        newOrderAddMode = true;
+        selectedOrder = null;
 	}
 
 	@Override
-	public void onDeailShowButton(OrderDTO item) {
-		// TODO Auto-generated method stub
-		
+	public void onDeailShowButton(OrderDTO order) {
+		selectedOrder = order;
+        newOrderAddMode = false;
 	}
-	
+
+    public Actor getActor() {
+        return actor;
+    }
+
+    public void setActor(Actor actor) {
+        this.actor = actor;
+    }
+
+    public List<OrderDTO> getOrderList() {
+        return orderList;
+    }
+
+    public void setOrderList(List<OrderDTO> orderList) {
+        this.orderList = orderList;
+    }
+
+    public boolean isNewOrderAddMode() {
+        return newOrderAddMode;
+    }
+
+    public void setNewOrderAddMode(boolean newOrderAddMode) {
+        this.newOrderAddMode = newOrderAddMode;
+    }
+
+    public OrderDTO getNewOrder() {
+        return newOrder;
+    }
+
+    public void setNewOrder(OrderDTO newOrder) {
+        this.newOrder = newOrder;
+    }
+
+    public boolean isOrderSelected() {
+        return selectedOrder != null;
+    }
 }
